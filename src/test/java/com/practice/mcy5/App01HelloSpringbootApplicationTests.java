e5969784dcf3c4abd3b3b67be6014ca4d07a4909
Mridul Choudhary <mridul.choudhary7@gmail.com>
2021-11-23 22:04:06 +0530

    Init commit

---
+++ src/test/java/com/practice/mcy5/App01HelloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mcy5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App01HelloSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
