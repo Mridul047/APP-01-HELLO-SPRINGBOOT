99e38a95c5401beb2683a4e5b1583eb7f9ef4965
Mridul Choudhary <mridul.choudhary7@gmail.com>
2021-11-24 11:00:35 +0530

    If applied this commit will partially implement getUsers .

--- src/main/java/com/practice/mcy5/rest/UserController.java
+++ src/main/java/com/practice/mcy5/rest/UserController.java
@@ -1,5 +1,6 @@
 package com.practice.mcy5.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +16,9 @@ public class UserController {
         return "Hello user , the time right now is " + new Date();
     }
 
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        List<User> usersList = new ArrayList<>();
+        return usersList;
+    }
 }
