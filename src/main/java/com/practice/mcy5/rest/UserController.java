c6de1057282545faab143504406db5cc3c259c61
Mridul Choudhary <mridul.choudhary7@gmail.com>
2021-11-24 10:06:00 +0530

    If applied this commit will add greet endpoint.

---
+++ src/main/java/com/practice/mcy5/rest/UserController.java
@@ -0,0 +1,18 @@
+package com.practice.mcy5.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/greeting")
+    public String greetUser(){
+        return "Hello user , the time right now is " + new Date();
+    }
+
+}
