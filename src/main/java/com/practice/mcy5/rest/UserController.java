a5ac38b9e04107e1aa5df63c2184d57fa5e658aa
Mridul Choudhary <mridul.choudhary7@gmail.com>
2021-11-24 12:33:11 +0530

    If applied this commit will  implement getUsers with User model.

--- src/main/java/com/practice/mcy5/rest/UserController.java
+++ src/main/java/com/practice/mcy5/rest/UserController.java
@@ -1,11 +1,14 @@
 package com.practice.mcy5.rest;
 
+import com.practice.mcy5.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
